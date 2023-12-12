54e47d67733a8e1a85f2c9cc0b8fdb85a5d9242e
kd14 <kd14@ford.com>
2023-12-12 18:02:53 +0530

    base code structure

---
+++ backend/producer/main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"strings"
+
+	"github.com/joho/godotenv"
+)
+
+func main() {
+	err := godotenv.Load("../.env")
+	if err != nil {
+		fmt.Println("Failed to Load ENV file...")
+	}
+
+	t := os.Getenv("TICKERS")
+	topics := strings.Split(t, ",")
+	for i, topic := range topics {
+		topics[i] = strings.Trim(strings.Trim(topic, "\\"), "\"")
+	}
+
+}
